f1d9829b510fec7e3515b1cf3e850047658052b2
vladonius <vladonius@gmail.com>
2019-05-28 13:21:08 +0200

    add back button

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {OverlayContainer} from '@angular/cdk/overlay';
 import {Router} from "@angular/router";
 import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
-
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -21,10 +21,13 @@ export class AppComponent {
   users: JSON;
   readonly ROOT_URL = 'http://127.0.0.1:5000/';
   isMobile: Observable<BreakpointState>;
-  constructor(overlayContainer: OverlayContainer, public router: Router,private breakpointObserver: BreakpointObserver) {
+  constructor(overlayContainer: OverlayContainer, public router: Router,private breakpointObserver: BreakpointObserver,private _location: Location) {
     // overlayContainer.getContainerElement().classList.add('unicorn-dark-theme');
     this.isMobile = this.breakpointObserver.observe(Breakpoints.Handset);
   }
 
+  goBack(){
+     this._location.back();
+  }
 
 }
