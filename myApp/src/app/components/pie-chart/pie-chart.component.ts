055bbe6c1734220d0252ee44f35cf4a267754ebd
YuliZa <zarichna19@gmail.com>
2019-06-10 00:05:15 +0200

    modified pie chart

--- myApp/src/app/components/pie-chart/pie-chart.component.ts
+++ myApp/src/app/components/pie-chart/pie-chart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType, ChartOptions } from 'chart.js';
 import {SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, Color} from 'ng2-charts';
+import {Observable} from "rxjs";
+import {Expense} from "../../models/expense";
+import {DashboardService} from "../../services/dashboard.service";
 
 @Component({
   selector: 'app-pie-chart',
@@ -8,27 +11,30 @@ import {SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsToolti
   styleUrls: ['./pie-chart.component.scss']
 })
 export class PieChartComponent implements OnInit {
+  private categories$: Observable<Expense[]>;
+
+
   // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
-  public pieChartLabels: Label[] = [['Income'], ['Account'], 'Expenses'];
-  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartLabels: Label;
+  public pieChartData: SingleDataSet;
   colors2: Color[] = [{
-    backgroundColor:'rgba(2, 2, 2, 1)',
+    backgroundColor:['green', 'blue', 'red', 'white'],
     borderColor: 'rgba(225,10,24,0.2)',
     pointBackgroundColor: 'rgba(225,10,24,0.2)',
     pointBorderColor: '#fff',
     pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(225,10,24,0.2)'},
-
+    pointHoverBorderColor: 'rgba(225,255,24,0.2)'}
   ];
-  colors: Color[] = [{backgroundColor: ['green', 'blue', 'white'],borderColor: 'rgba(4,10,24,.4)'}];
+  colors: Color[] = [{backgroundColor: ['green', 'blue', 'red', 'white'],borderColor: 'rgba(4,10,24,.4)'}];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
+  public expenses = 0;
 
-  constructor() {
+  constructor(private dashboardService: DashboardService) {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
   }
@@ -36,6 +42,32 @@ chartColors(){
     return this.colors;
 }
   ngOnInit() {
-  }
+    // display data in pie chart
+    let categoryNames = [];
+    let categorySpendings = [];
+    let expensesSum = 0;
+    this.categories$ = this.dashboardService.getAll('expense');
+    this.categories$.forEach(categories => {
+      categories.forEach( category => {
+        categoryNames.push(category.name);
+        categorySpendings.push(category.spent_amount);
+            console.log("1", category.spent_amount);
 
+        expensesSum = expensesSum + category.spent_amount;
+            console.log("2", expensesSum);
+
+      });
+      console.log(categories[0].name);
+      console.log(categories);
+      console.log(expensesSum);
+      this.expenses = expensesSum;
+    });
+
+
+   this.pieChartLabels = categoryNames;
+   this.pieChartData = categorySpendings;
+
+
+  }
 }
+
